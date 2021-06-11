3674fda860d46b8499b2948472d85fe4db98553a
valgring95 <33157@student.pwsztar.edu.pl>
2021-06-11 10:42:52 +0200

    kolos

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
+
 import './App.css';
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Posts from "./components/posts";
+import Ddhome from "./components/ddhome";
 
 function App() {
-
-  return (
-      <div className="container-fluid">
-        <div className="container">
-            <div className="content">
-                <Switch>
-                    <Route path="/" exact component={Posts} />
-                </Switch>
+    return (
+        <div className="container-fluid">
+            <div className="container">
+                <div className="content">
+                    <Switch>
+                        <Route path="/posts"
+                               component={Posts} />
+                        <Route path="/" exact component={Posts} />
+                    </Switch>
+                </div>
             </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default App;
--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -40,11 +40,10 @@ class Posts extends Component {
                 {items.map((item, key) => {
                     return <div key={key}>
                         <div className="card">
-                            <img className="card-img-top" src="..." alt="Card image cap" />
+                            <img className="card-img-top" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt="Card image cap" />
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make
-                                        up the bulk of the card's content.</p>
+                                    <h5 className="card-title">Przykładowe zdjęcie</h5>
+                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                                     <a href="#" className="btn btn-primary">Go somewhere</a>
                                 </div>
                         </div>
